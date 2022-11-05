9563220b47e6ccb47ec91dc1ad1cedc8f0bae560
Artur <sultanovartur1873@gmail.com>
2022-11-05 14:03:50 +0300

    [add]: config pacage

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"burmachine/configService/config"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	cfgPath := flag.String("config", "./config.yaml", "Path to yaml configuration file")
+
+	flag.Parse()
+
+	if *cfgPath == "" {
+		log.Fatalln("Path to configuration file was not provided")
+	}
+	conf := config.NewConfigStruct()
+	err := conf.LoadConfig(*cfgPath)
+	if err != nil {
+		log.Fatalln("Config loading error")
+	}
+	mux := http.NewServeMux()
+	fmt.Println(mux)
+}
---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io"
+	"os"
+)
+
+type conf struct {
+	Addr string `yaml:"port"`
+}
+
+func NewConfigStruct() *conf {
+	return &conf{}
+}
+
+func (c *conf) LoadConfig(fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return fmt.Errorf("Failed open config file :%v", err)
+	}
+	defer file.Close()
+	read, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("Failed reading file :%v", err)
+	}
+	err = yaml.Unmarshal(read, c)
+	if err != nil {
+		return fmt.Errorf("Yaml unmarshalling error :%v", err)
+	}
+
+	return nil
+}
